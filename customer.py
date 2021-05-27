63d1798bef258d11261ee9b66b072fafbab0affc
diegofalconc <diegofalconcostilla94@gmail.com>
2021-05-27 14:37:39 -0700

    Class declaration

---
+++ customer.py
@@ -0,0 +1,18 @@
+class Customer():
+    def __init__(self, customername, address, email, membershipcardid)
+        self.customername=customername
+        self.address=address
+        self.email = email
+        self.membershipcardid = membershipcardid
+    
+    def getCustomerName(self):
+        return self.customername 
+    
+    def getAddress(self):
+        return self.address
+    
+    def getEmail(self):
+        return self.email
+    
+    def getMembershipCardId(self):
+        return self.membershipcardid
---
+++ sale.py
@@ -0,0 +1,13 @@
+import datetime
+
+class Sale():
+    def __init__(self, items, dateCreated, status, totalPrice)
+        self.items=items
+        self.dateCreated=datetime.date.today()
+        self.status = status
+        self.totalPrice = totalPrice
+    
+    def totalPrice(items):
+        return self.lockid 
+    
+    
